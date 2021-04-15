fbddb6127b64f08bbb21cb959bbef8e69c4ee1cb
Yulim2 <jylimmm@gmail.com>
2021-04-15 15:04:30 +0900

    옆으로 정렬, 고정

--- 2021_04_09/반복문/반복문_4.cpp
+++ 2021_04_09/반복문/반복문_4.cpp
@@ -3,9 +3,9 @@
 int main()
 {
 	int i, j;
-	for(i=2; i<=9; i++){
-		for(j=1; jw<=9; j++)
-		printf("%d * %d = %d\n", i, j, i*j);
+	for(i=1; i<=9; i++){
+		for(j=2; j<=9; j++)
+		printf("%d * %d = %2d\t", j, i, j*i);
 		printf("\n");
 	}
 }
