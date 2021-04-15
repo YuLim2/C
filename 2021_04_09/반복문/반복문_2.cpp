53e630706d2dca49758a0a3913db849f0a7819da
Yulim2 <jylimmm@gmail.com>
2021-04-15 14:52:40 +0900

    2021_04_15 C언어수업 내용

---
+++ 2021_04_09/반복문/반복문_2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+
+    for(int i=1; i<=100; i++)
+        printf("%d",i);
+        //sum+=i;
+ //   }
+   // printf("\n1~100까지 합은 %d입니다. \n");
+return 0;
+}
---
+++ 2021_04_09/반복문/반복문_4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, j;
+	for(i=2; i<=9; i++){
+		for(j=1; jw<=9; j++)
+		printf("%d * %d = %d\n", i, j, i*j);
+		printf("\n");
+	}
+}
