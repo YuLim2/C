0f456e2c170d79c6849897ae079956b825c649ad
Yulim2 <jylimmm@gmail.com>
2021-05-06 15:13:03 +0900

    2021_05_06 C언어 수업 내용

---
+++ 2021_05_06/DSlib.h
@@ -0,0 +1,10 @@
+//DS Library
+
+void printDS_Name(void){
+	printf("Busan SW\n");
+}
+
+void printDS_ID(void){
+	printf("2021\n");
+	
+}
---
+++ 2021_05_06/call.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "DSlib.h" 
+int main()
+{
+	printf("my name =");
+	printDS_Name();
+	
+	printf("my student ID =");
+	printDS_ID();
+	
+	return 0;
+}
